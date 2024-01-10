15ec19b3dc7df2d492ec6141f123f4ac691550df
rajaei <mr.rajjaei@gmail.com>
2024-01-10 09:43:16 +0330

    first init

---
+++ SimpleDetector/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace SimpleDetector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.checkBoxAskMe = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sample app for the DriveDetector class";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(21, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(463, 47);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "In the list below information about removable drives arriving and being removed w" +
+    "ill appear.";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(25, 102);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(443, 68);
+            this.listBox1.TabIndex = 2;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonClose.Location = new System.Drawing.Point(387, 238);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(100, 28);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // checkBoxAskMe
+            // 
+            this.checkBoxAskMe.AutoSize = true;
+            this.checkBoxAskMe.Location = new System.Drawing.Point(13, 238);
+            this.checkBoxAskMe.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBoxAskMe.Name = "checkBoxAskMe";
+            this.checkBoxAskMe.Size = new System.Drawing.Size(277, 20);
+            this.checkBoxAskMe.TabIndex = 4;
+            this.checkBoxAskMe.Text = "Ask me before drive can be disconnected";
+            this.checkBoxAskMe.UseVisualStyleBackColor = true;
+            this.checkBoxAskMe.CheckedChanged += new System.EventHandler(this.checkBoxAskMe_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "url File list:>";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Content File:>";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Enabled = false;
+            this.txtURL.Location = new System.Drawing.Point(106, 178);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(317, 22);
+            this.txtURL.TabIndex = 7;
+            // 
+            // txtContent
+            // 
+            this.txtContent.Enabled = false;
+            this.txtContent.Location = new System.Drawing.Point(121, 209);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(302, 22);
+            this.txtContent.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonClose;
+            this.ClientSize = new System.Drawing.Size(500, 273);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBoxAskMe);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "USB API File sender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.CheckBox checkBoxAskMe;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.TextBox txtContent;
+    }
+}
+
---
+++ SimpleDetector/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using Dolinay;
+using System.IO;
+using NLog;
+using NLog.Targets;
+using RestSharp;
+
+namespace SimpleDetector
+{
+    public partial class Form1 : Form
+    {
+        private DriveDetector driveDetector = null;
+        Logger logger = LogManager.GetLogger("simple");
+        public Form1()
+        {
+            InitializeComponent();
+            driveDetector = new DriveDetector();
+            driveDetector.DeviceArrived += new DriveDetectorEventHandler(OnDriveArrived);
+            driveDetector.DeviceRemoved += new DriveDetectorEventHandler(OnDriveRemoved);
+            driveDetector.QueryRemove += new DriveDetectorEventHandler(OnQueryRemove);
+
+            var target = new FileTarget();
+            target.FileName = "${basedir}/output.log";
+
+            NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, LogLevel.Info);
+
+            
+            
+        }
+
+        // Called by DriveDetector when removable device in inserted 
+        private void OnDriveArrived(object sender, DriveDetectorEventArgs e)
+        {
+            // Report the event in the listbox.
+            // e.Drive is the drive letter for the device which just arrived, e.g. "E:\\"
+            string s = "Drive arrived " + e.Drive;
+            listBox1.Items.Add(s);
+            logger.Info(s);
+
+            CheckAndSendF(e.Drive);
+
+            // If you want to be notified when drive is being removed (and be able to cancel it), 
+            // set HookQueryRemove to true 
+            if ( checkBoxAskMe.Checked )   
+                e.HookQueryRemove = true;                 
+        }
+
+        public string SendString(RestSharp.Method myMethod,String Body, String PostToDeviceAdress, String Destination)
+        {
+            string result = string.Empty;
+            var client = new RestClient(PostToDeviceAdress); // "http://172.20.32.21:8085/datadiod");
+            var request = new RestRequest();
+            request.Method = myMethod; // Method.Post;
+            request.AddHeader("Content-Type", "application/json");
+
+            var body = Body;
+
+            StreamWriter sw = new StreamWriter("body.txt", true);
+            sw.WriteLine(body);
+            sw.Close();
+
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            var response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            return result;
+        }
+        private void CheckAndSendF(string DriveName)
+        {
+            //TODO: check file and log
+            //TODO: send file if exist
+        }
+
+        // Called by DriveDetector after removable device has been unpluged 
+        private void OnDriveRemoved(object sender, DriveDetectorEventArgs e)
+        {
+            // TODO: do clean up here, etc. Letter of the removed drive is in e.Drive;
+            
+            // Just add report to the listbox
+            string s = "Drive removed " + e.Drive;
+            listBox1.Items.Add(s);
+            logger.Info(s);
+        }
+
+        // Called by DriveDetector when removable drive is about to be removed
+        private void OnQueryRemove(object sender, DriveDetectorEventArgs e)
+        {
+            // Should we allow the drive to be unplugged?
+            if (checkBoxAskMe.Checked)
+            {
+                if (MessageBox.Show("Allow remove?", "Query remove",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    e.Cancel = false;       // Allow removal
+                else
+                    e.Cancel = true;        // Cancel the removal of the device  
+            }
+        }
+        
+
+        // User checked the "Ask me before drive can be disconnected box"        
+        private void checkBoxAskMe_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxAskMe.Checked)
+            {
+                // Is QueryRemove enabled? 
+                // If not, we will enable it for the drive which is selected 
+                // in the listbox. 
+                // If the listbox is empty, no drive has been detected yet so do nothing now.
+                if (!driveDetector.IsQueryHooked && listBox1.Items.Count > 0)
+                {
+                    if (listBox1.SelectedItem == null)
+                    {
+                        MessageBox.Show("Please choose the drive for which you wish to be asked in the list (select its message).");
+                        checkBoxAskMe.Checked = false;
+                        return;
+                    }
+
+                    bool ok = false;
+                    string s = (string)listBox1.SelectedItem;
+                    int n = s.IndexOf(':');
+                    if (n > 0)
+                    {
+                        s = s.Substring(n - 1, 3);  // Gets drive letter from the message, (e.g. "E:\\") 
+
+                        // Tell DriveDetector to monitor this drive
+                        ok = driveDetector.EnableQueryRemove(s);
+                    }
+
+                    if (!ok)
+                        MessageBox.Show("Sorry, for some reason notification for QueryRemove did not work out.");
+
+                }
+
+            }
+            else
+            {
+                // "unchecked" the box so disable query remove message
+                if (driveDetector.IsQueryHooked)
+                    driveDetector.DisableQueryRemove();
+            }                        
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtContent.Text = Properties.Resources.ContentFile.ToString();
+            txtURL.Text = Properties.Resources.UrlPathFile.ToString();
+        }
+    }
+}
